423e5fa7a666a4d9cad248291a554fd710449b76
Agerup <joag@fc-ubu1-043.ipp-hgw.mpg.de>
2021-07-30 16:47:18 +0200

    Added last algo to timeseries. Ready for making data for denoising.

--- grid.py
+++ grid.py
@@ -17,29 +17,21 @@ class grid:
         dgrid = np.asarray( ds.R_bounds.mean(dim=("delta_r", "delta_theta", "delta_phi")) )[inc_r[0]:inc_r[1],
                                                                                             inc_t[0]:inc_t[1],
                                                                                             inc_p[0]:inc_p[1]].shape
-        #regenerating dimensions to add periodic boundary conditions in theta dir
-#         dgrid = list(dgrid)
-#         dgrid[1] += 1
-#         dgrid = tuple(dgrid)
-#         print("dim grid",dgrid)
         
         self.R, self.theta, self.phi, self.tor_z = np.ones(dgrid)*np.nan, np.ones(dgrid)*np.nan, np.ones(dgrid)*np.nan, np.ones(dgrid)*np.nan
         
         self.R = np.asarray( ds.R_bounds.mean(dim=("delta_r", "delta_theta", "delta_phi")) )[inc_r[0]:inc_r[1],
                                                                                              inc_t[0]:inc_t[1],
                                                                                              inc_p[0]:inc_p[1]]
-        # set the periodic BC for R vals
-#         self.R[:,-1,:] = self.R[:,0,:]
+
         
         self.tor_z = np.asarray(ds.z_bounds.mean(dim=("delta_r", "delta_theta", "delta_phi")))[inc_r[0]:inc_r[1],
                                                                                                inc_t[0]:inc_t[1],
                                                                                                inc_p[0]:inc_p[1]]
-        # set the periodic BC for z vals
-#         self.tor_z[:,-1,:] = self.tor_z[:,0,:]
+
         
         self.phi = np.asarray( ds.phi_bounds.mean(dim="delta_phi") )[inc_p[0]:inc_p[1]]
-        # set the periodic BC for phi values
-#         self.phi[:,-1,:] = self.phi[:,0,:]
+
         
         self.tor_x = np.asarray( self.R * np.cos(self.phi) )
         self.tor_y = np.asarray( self.R * np.sin(self.phi) )
@@ -47,16 +39,16 @@ class grid:
             ngrid_cart = 100
         
         ivl_p = int(inc_p[1] - inc_p[0])
-        #chunking = 4
-        #chunkingids = 0,0,0
-        """self.cart_x, self.cart_y, self.cart_z = np.meshgrid(np.linspace(np.min(self.tor_x),np.max(self.tor_x)[ngrid_cart // chunking*chunkingids[0]:ngrid_cart // chunking*(chunkingids[0]+1)], ngrid_cart), 
-                                                            np.linspace(np.min(self.tor_y),np.max(self.tor_y), ngrid_cart), 
-                                                            np.linspace(np.min(self.tor_z),np.max(self.tor_z), ngrid_cart),
-                                                            indexing = "ij")"""
+        chunking = 2
+        chunkingids = (2,3,4)
         self.cart_x, self.cart_y, self.cart_z = np.meshgrid(np.linspace(np.min(self.tor_x),np.max(self.tor_x), ngrid_cart), 
                                                             np.linspace(np.min(self.tor_y),np.max(self.tor_y), ngrid_cart), 
                                                             np.linspace(np.min(self.tor_z),np.max(self.tor_z), ngrid_cart),
                                                             indexing = "ij")
+        """self.cart_x, self.cart_y, self.cart_z = np.meshgrid(np.linspace(np.min(self.tor_x),np.max(self.tor_x), ngrid_cart), 
+                                                            np.linspace(np.min(self.tor_y),np.max(self.tor_y), ngrid_cart), 
+                                                            np.linspace(np.min(self.tor_z),np.max(self.tor_z), ngrid_cart),
+                                                            indexing = "ij")"""
      
         self.cartflat = np.ones((self.cart_x.shape[0]*self.cart_x.shape[1]*self.cart_x.shape[2], 3) )*np.nan
         self.cartflat[:,0] = self.cart_x.flatten()
--- makemesh_indices.py
+++ makemesh_indices.py
@@ -1,4 +1,4 @@
-def make_parammesh_vtk_indices(grid_obj, sgrid, skip_coarsegrid = 10):
+def make_parammesh_vtk_indices(grid_obj, sgrid, skip_coarsegrid = 10, sp = (29,2,16), tolsqrd = 1e-8):
     """
     Args:
      - grid_obj, grid object defined in one of the cells below
@@ -17,96 +17,78 @@ def make_parammesh_vtk_indices(grid_obj, sgrid, skip_coarsegrid = 10):
     
     # make the mehsgrid for the parameter we want to interpolate form the original data
     # + (2,8) represents (indices/weights, vertex)
-    parammesh_indices = np.nan*np.ones(grid_obj.cart_x.shape + (2,8), dtype=np.int32)
-    
+    parammesh_indices = -np.ones(grid_obj.cart_x.shape + (8,), dtype=np.int32 )
+    parammesh_weights = np.nan*np.ones(grid_obj.cart_x.shape + (8,), dtype=np.float32)
     # pass initial/default values to the find_cell function
     cell = None
     cid = int(6e3)
-    tol2 = 1e-3
+    tol2 = tolsqrd
+    print(tol2)
     subid = vtk.reference(-1)
-    pcoord = np.zeros((3))
-    weights = np.zeros(8)
-    # iterate through points in the cartesian grid
-    
-    #making coarse grid to speed up the find point in cell stage
-    numc = 20
-    coarse = np.zeros((numc, numc, numc), dtype=bool)
-    #spatial_toroidal = np.array([grid_obj.__dict__[d] for d in ("R", "tor_z", "phi")])
-    #minmax = [np.array([func(grid_obj.__dict__[d]) for d in ("R", "tor_z", "phi")]) for func in [np.min, np.max]]
-    spatial_toroidal = np.array([grid_obj.__dict__["tor_"+d] for d in "xyz"])
-    minmax = [np.array([func(grid_obj.__dict__["tor_"+d]) for d in "xyz"]) for func in [np.min, np.max]]
-    offset = minmax[0]
-    dist = (minmax[1] - minmax[0]+1e-6)/numc
-    for i,j,k in itertools.product(*[range(x-1) for x in grid_obj.tor_x.shape]):
-        block = spatial_toroidal[:,i:i+2, j:j+2, k:k+2].reshape(3, 8)
-        minmax = [func(block, axis=1) for func in [np.min, np.max]]
-        indices = [((m - offset) / dist).astype(int) for m in minmax]
-        for ijk in itertools.product(*[range(a,b+1) for a,b in zip(*indices)]):
-            coarse[ijk] = True
+    pcoord = np.zeros(3)
+    weights = np.zeros(8, dtype=np.float32)
     
     # generate the shape of the cellular emc3 grid
     grid_of_cells_shape = (grid_obj.tor_x.shape[0]-1, grid_obj.tor_x.shape[1]-1, grid_obj.tor_x.shape[2]-1)
     
     # for storing cell ids of the emc3 grid to each grid point in the cartesian grid
-    cids = np.zeros(grid_obj.cart_x.shape)-2
-    cs = [117,4,64] # this is now hardcoded, need to find a generalized solution
+    cids = np.zeros(grid_obj.cart_x.shape, dtype=np.int32)-2
+    cs = sp # this is now hardcoded, need to find a generalized solution
     todo = [tuple(list(cs))] #starting index of point to search for in the cells to search
+    found = False
     while todo:
         cur = todo.pop()
         #find the cell p is interior to
         cid = sgrid.find_cell([grid_obj.cart_x[cur], grid_obj.cart_y[cur], grid_obj.cart_z[cur]], 
                               cell, cid, tol2, subid, pcoord, weights)
         cids[cur] = cid
-        # if find_cell returns -1 there is either a numerical error or it did not find the point in that cell
         if cid >= 0:
+            found = True
             for dist in itertools.product(range(-1,2),repeat=3): #iterate through neighbours
                 nxt = tuple(a+b for a,b in zip(dist, cur))
-                if nxt > grid_obj.cart_x.shape or nxt[1:]>grid_obj.cart_x.shape[1:] or nxt[2:]>grid_obj.cart_x.shape[2:]: #is out of bounds:
+                outofbounds = False
+                for i in range(3):
+                    if nxt[i]>=grid_obj.cart_x.shape[i]:
+                        outofbounds = True
+                if outofbounds:
                     continue
                 if cids[nxt] == -2:
                     cids[nxt] = -3
                     todo.append(nxt)
-                    print("imhere")
-            #define the cartesian point
-            p = [grid_obj.cart_x[cur], grid_obj.cart_y[cur], grid_obj.cart_z[cur]]
             # unravel index of each vertex of the cell found from the toroidal grid
             ir, it, ip = np.unravel_index(cid, grid_of_cells_shape)
             idx = np.zeros((2,2,2), dtype=int)
             for d_ir, dit, dip in itertools.product(range(2), repeat=3):
                 idx[d_ir, dit, dip] = np.ravel_multi_index((ir+d_ir,it+dit,ip+dip), grid_obj.tor_x.shape)
             idx.shape = 8
-            # find coordinate indices of the point in the cartesian grid structure with cartesian indices
-            coord_idx = np.argwhere((grid_obj.cart_x == p[0]) & 
-                                    (grid_obj.cart_y == p[1]) & 
-                                    (grid_obj.cart_z == p[2]))[0]
             #represents the indices of the flattened parametermesh on the emc3 grid structure
-            parammesh_indices[coord_idx[0], coord_idx[1], coord_idx[2],0] = idx
+#             print(parammesh_indices.shape, parammesh_indices.dtype, cur, type(cur), idx, idx.dtype, weights, weights.dtype)
+            parammesh_indices[cur] = idx
             # represents the weights corresponding to the parameter indices above
-            parammesh_indices[coord_idx[0], coord_idx[1], coord_idx[2],1] = weights
-    return parammesh_indices
+            parammesh_weights[cur] = weights
+        #if find_cell returns -1 there is either a numerical error or it did not find the point in that cell
+        if not found:
+            print("guessing startpoint")
+            cs = tuple(np.random.randint(low = 0, high = max(grid_obj.cart_x.shape), size = 3))
+            todo.append(cs)
+    return parammesh_indices, parammesh_weights
 
-def from_indices_to_paramvals(indices, param_torgrid):
+def from_indices_to_paramvals(idxs, weights, param_torgrid):
     """
     Current problem is that it is not identical to the former method where the the parametervalues are extracted
     rather than the indices of the toroidal grid.
     """
     #imports for importing to external file later
     import numpy as np
-    
     # shape pmesh as (50,50,50), if ngrid = 50
-    pmesh = np.ones(indices.shape[:3])*np.nan
-    # shape = (50, 50, 50, 8)
-    weights = indices[:,:,:,1]
-    # goal to convert idxs to parametervalues
-    idxs = indices[:,:,:,0]
+    pmesh = np.ones(idxs.shape[:3], dtype=np.float64)*np.nan
     # make a flattened array of sum of indices of all vertices in the cell, if any is nan then discard the cell
     irav = np.sum(idxs, axis = 3).ravel()
-    # store indices of indices which are numbers in a flattened structure
+    # store indices of indices which are numbers in a flattened structjupyter labextension install @jupyter-widgets/jupyterlab-managerure
     isnotnan = np.where(np.isnan(irav) == False)[0]
     # array of indexes that is not nanvalued
     notnanidxs = np.asarray(idxs.reshape(idxs.shape[0]**3,8)[isnotnan], dtype=np.int32)
-    
     pmesh_re = pmesh.ravel()
     pmesh_re[isnotnan] = np.sum(param_torgrid.ravel()[notnanidxs] * weights.reshape(weights.shape[0]**3, 8)[isnotnan], axis = 1)
-    pmesh = pmesh_re.reshape(indices.shape[:3])
+    pmesh = pmesh_re.reshape(idxs.shape[:3])
     return pmesh   
